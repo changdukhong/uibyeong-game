708035203c7a3f00da560882ebaf81111c744317
changdukhong <60304817+changdukhong@users.noreply.github.com>
2025-09-30 14:03:08 +0900

    Update script.js

--- script.js
+++ script.js
@@ -331,7 +331,8 @@ window.addEventListener('resize', adjustCharacterBottom);
 document.addEventListener('DOMContentLoaded', startGame);
 
 const tickerText = document.getElementById('ticker-text');
-tickerText.textContent = "의병장 할아버지, 힘내세요! 왜장(가등청정) 반드시 잡을 수 있습니다! 14세손 손자: 시현,이안,윤재";
+tickerText.textContent = "의병장 할아버지, 힘내세요! 왜장(가등청정) 반드시 잡을 수 있습니다! 14세손: 시현,이안,윤재";
+
 
 
 

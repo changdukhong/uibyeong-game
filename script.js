a436bf0c1126dbc57f57d53cf8a0d66966fa84e1
changdukhong <60304817+changdukhong@users.noreply.github.com>
2025-10-02 04:15:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -132,7 +132,8 @@ function spawnAngledArrow() {
 if (t >= duration) {
   clearInterval(motion);
 
-  const screenHeight = window.innerHeight;
+  // const screenHeight = window.innerHeight;
+  const screenHeight = 985;
   const isNearBottom = y >= screenHeight - 40;
   const isMidAngle = angle >= 45 && angle <= 135;
 
@@ -499,6 +500,7 @@ document.addEventListener('DOMContentLoaded', startGame);
 
 const tickerText = document.getElementById('ticker-text');
 tickerText.textContent = "장군! 적군이 도망갑니다. 적장을 잡으러 가자..., 와!, 와!, 의병장 할아버지, 힘내세요! 왜장(가등청정)을 반드시 잡아 주세요! ";
+
 
 
 
